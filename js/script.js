a658fa51f15acca49c0cc9e4774b76c208a7b9b6
Andrea <andreapirrello1990@gmail.com>
2020-07-17 12:49:36 +0200

    aggiunto codice per ricerca utente

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-
+// ricerca utente 
+  $(".search-bar__input").keydown(function() {
+    var inputRicerca = $(this).val().toLowerCase();
+    $(".conversation").each(function() {
+      var nomiConversazioni = $(this).find(".title__name").text().toLowerCase();
+      if (nomiConversazioni.includes(inputRicerca)) {
+        $(this).fadeIn();
+      } else {
+        $(this).fadeOut();
+      }
+    });
+  });
