5cbfad6cf6ba592bfc8ebf35148df3d72e07d47f
Andrea <andreapirrello1990@gmail.com>
2020-07-17 09:40:42 +0200

    sfondo

--- js/script.js
+++ js/script.js
@@ -1,15 +1,1 @@
-$(document).ready(function() {
 
-  // FUNZIONE INVIO MESSAGGIO CON CLICK MOUSE
-  $(document).on('click', '.message_send', function() {
-    invioMessaggio();
-  });
-
-  // FUNZIONE INVIO MESSAGGIO CON TASTO INVIO TASTIERA
-  $('.input_message').keypress(
-    function() {
-      if (event.which == 13 || event.keyCode == 13) {
-        invioMessaggio();
-      }
-    }
-  );
